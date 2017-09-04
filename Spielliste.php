fed0ab2729f89be0c49dc9485e94457c59a351bd
Marexa <Marexa@ewe.de>
2017-09-04 21:31:45 +0200

    Spielliste aktualisiert

--- Spielliste.php
+++ Spielliste.php
@@ -3,12 +3,12 @@ $connection = mysqli_connect ("localhost", "root", "123456", "Test" )
 or die ("keine Verbindung möglich. Benutzername oder Passwort sind falsch");
 
 
-if(isset($_POST['Sportart'],$_POST['Spiel_ID'],$_POST['ToreTeam1'],$_POST['ToreTeam2'])){
-    $Sportart = $_POST['Sportart'];
-	$Sportart1 = $Sportart."_data";
-	$Spiel_ID = $_POST['Spiel_ID'];
-    $ToreTeam1 = $_POST['ToreTeam1'];
-	$ToreTeam2 = $_POST['ToreTeam2'];
+if(isset($_POST['sport'],$_POST['spielId'],$_POST['t1'],$_POST['t2'])){
+    $Sportart = $_POST['sport'];
+	$Sportart1 = $Sportart."_spielplan";
+	$Spiel_ID = $_POST['spielId'];
+    $ToreTeam1 = $_POST['t1'];
+	$ToreTeam2 = $_POST['t2'];
 	$Tore = $ToreTeam1.":".$ToreTeam2;
 	$sql = "UPDATE $Sportart1 SET Ergebnis = '$Tore' WHERE Spiel_ID = '$Spiel_ID'";
 	$result = mysqli_query($connection, $sql);
