8c54196ca3d3f6a0c4fd7d0273b2d4f5151853be
Marexa <Marexa@ewe.de>
2017-09-04 21:30:21 +0200

    Spieleintrag aktualisiert

--- Spieleintrag.php
+++ Spieleintrag.php
@@ -5,7 +5,7 @@ or die ("keine Verbindung möglich. Benutzername oder Passwort sind falsch");
 
 if(isset($_POST['Sportart'])){
 	$Sportart = $_POST['Sportart'];
-	$Sportart1 = $Sportart."_data";
+	$Sportart1 = $Sportart."_spielplan";
 	$sql = "SELECT * FROM $Sportart1";
 	$result = mysqli_query($connection, $sql);
 	if($result == ""){
@@ -14,7 +14,7 @@ if(isset($_POST['Sportart'])){
 	else{	
 		$response = array();
 		while($row = mysqli_fetch_array($result)){
-		array_push($response, array('Spiel_ID' =>$row[0],'Uhrzeit' =>$row[1],'Halle' =>$row[2], 'Mannschaft1'=>$row[3], 'Mannschaft2'=>$row[4], 'Ergebnis'=>$row[5], 'Schiedsrichter'=>$row[6]));
+		array_push($response, array('Spiel_ID' =>$row[0],'Uhrzeit' =>$row[1],'Halle' =>$row[2], 'Mannschaft1'=>$row[3], 'Mannschaft2'=>$row[4], 'Ergebnis'=>$row[5], 'Schiedsrichter'=>$row[6], 'Gruppe'=>$row[7]));
 		}
 		echo json_encode($response);
 	}
